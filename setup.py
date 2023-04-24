e0537f418ab8a3ddf4617640fd3ca6e8afc24fc8
Tan Long <tanloong@foxmail.com>
2023-04-24 12:58:15 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
+import sys
+
 import setuptools
 from setuptools.command.install import install as install_
 
 class InstallCommand(install_):
     def run(self):
-        import subprocess
-        subprocess.call(["python", "-m", "pip", "install", "stanza", "spacy>=3.5.1"])
-        import stanza
-        import spacy
-        stanza.download("en", resources_url="stanfordnlp")
-        spacy.cli.download("en_core_web_sm")
         install_.run(self)
+        from stanza import download as download_stanza
+        from spacy.cli import download as download_spacy
+        download_stanza("en", resources_url="stanfordnlp")
+        download_spacy("en_core_web_sm", False, False, "--exists-action", "i", "--user", "--no-deps")
 
 with open("./README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
