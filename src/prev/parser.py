ed664e63519f6550378c32abeb0cb47f8d28f56d
Tan Long <tanloong@foxmail.com>
2023-04-25 20:53:16 +0800

    feat: can match combined preposition phrases now (e.g. I rummage in the suitcase for a tie.)

--- src/prev/parser.py
+++ src/prev/parser.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
-import logging
 import json
-from typing import Optional
+import logging
 import os
+from typing import Optional
+
 from spacy.tokens import Doc as Doc_spacy
 from stanza.models.common.doc import Document as Doc_stanza
 
@@ -15,7 +16,7 @@ class DependencyParser:
         self.is_stanza_initialized = False
         self.is_spacy_initialized = False
 
-    def ensure_stanza_initialized(func):
+    def ensure_stanza_initialized(func):  # type:ignore
         def wrapper(self, *args, **kwargs):
             if not self.is_stanza_initialized:
                 logging.info("Initializing Stanza...")
@@ -29,35 +30,39 @@ class DependencyParser:
                     download_method=None,  # type:ignore
                 )
                 self.is_stanza_initialized = True
-            return func(self, *args, **kwargs) #type:ignore
+
+            return func(self, *args, **kwargs)  # type:ignore
+
         return wrapper
 
-    def ensure_spacy_initialized(func):
+    def ensure_spacy_initialized(func):  # type:ignore
         def wrapper(self, *args, **kwargs):
             if not self.is_spacy_initialized:
                 logging.info("Initializing spaCy...")
                 import spacy
 
-                self.nlp_spacy = spacy.load(
-                    "en_core_web_sm", exclude=["ner", "attribute_ruler", "tagger"]
-                )
+                self.nlp_spacy = spacy.load("en_core_web_sm", exclude=["ner", "tagger"])
                 self.is_spacy_initialized = True
-            return func(self, *args, **kwargs) #type:ignore
+
+            return func(self, *args, **kwargs)  # type:ignore
+
         return wrapper
 
-    @ensure_stanza_initialized #type:ignore
-    def _postag(self, text:str):
-        return self.nlp_stanza(text) #type:ignore
+    @ensure_stanza_initialized  # type:ignore
+    def _postag(self, text: str):
+        logging.info(f"POS tagging...")
+        return self.nlp_stanza(text)  # type:ignore
 
     def postag(self, text: str, ifile_prefix: str):
         ofile_postagged = ifile_prefix + "_pos-tagged.json"
         if os.path.exists(ofile_postagged) and not self.is_refresh:
             logging.info(f"{ofile_postagged} already exists. POS tagging skipped.")
+
             with open(ofile_postagged, "r") as f:
                 doc_stanza = Doc_stanza(json.load(f))
         else:
-            logging.info(f"POS tagging...")
             doc_stanza = self._postag(text)
+
             logging.info(f"Saving POS tagged file in {ofile_postagged}.")
             with open(ofile_postagged, "w") as f:
                 f.write(json.dumps(doc_stanza.to_dict()))  # type:ignore
@@ -66,6 +71,14 @@ class DependencyParser:
     def stanza2spacy(self, doc_stanza):
         logging.info("Converting doc_stanza to doc_spacy...")
         words_stanza = list(doc_stanza.iter_words())  # type:ignore
+
+        words = [word.text for word in words_stanza]
+        pos = [word.pos for word in words_stanza]
+        tags = [word.xpos for word in words_stanza]
+        spaces = [
+            w.end_char != n.start_char for w, n in zip(words_stanza[:-1], words_stanza[1:])
+        ] + [False]
+
         sent_start_ids = []
         current_id = 0
         for sentence in doc_stanza.sentences:  # type:ignore
@@ -74,31 +87,34 @@ class DependencyParser:
         is_sent_start = [False for _ in words_stanza]
         for sent_start_id in sent_start_ids:
             is_sent_start[sent_start_id] = True
+
         doc_spacy = Doc_spacy(
-            self.nlp_spacy.vocab, #type:ignore
-            words=[word.text for word in words_stanza],
-            pos=[word.pos for word in words_stanza],
-            tags=[word.xpos for word in words_stanza],
-            spaces=[
-                w.end_char != n.start_char for w, n in zip(words_stanza[:-1], words_stanza[1:])
-            ]
-            + [False],
+            self.nlp_spacy.vocab,  # type:ignore
+            words=words,
+            pos=pos,
+            tags=tags,
+            spaces=spaces,
             sent_starts=is_sent_start,  # type:ignore
         )
         return doc_spacy
 
-    @ensure_spacy_initialized #type:ignore
+    @ensure_spacy_initialized  # type:ignore
     def _depparse(self, text: str, ifile_prefix: str):
         ofile_depparsed = ifile_prefix + "_dep-parsed.json"
         if os.path.exists(ofile_depparsed) and not self.is_refresh:
             logging.info(f"{ofile_depparsed} already exists. Dependency parsing skipped.")
+
             with open(ofile_depparsed, "r") as f:
-                doc_spacy = Doc_spacy(self.nlp_spacy.vocab).from_json(json.load(f)) #type:ignore
+                doc_spacy = Doc_spacy(self.nlp_spacy.vocab).from_json(  # type:ignore
+                    json.load(f)
+                )
         else:
             doc_stanza = self.postag(text, ifile_prefix)  # type:ignore
             doc_spacy = self.stanza2spacy(doc_stanza)
+
             logging.info(f"Dependency parsing...")
-            doc_spacy = self.nlp_spacy(doc_spacy) #type:ignore
+            doc_spacy = self.nlp_spacy(doc_spacy)  # type:ignore
+
             logging.info(f"Saving parse trees in {ofile_depparsed}.")
             with open(ofile_depparsed, "w") as f:
                 f.write(json.dumps(doc_spacy.to_json()))
@@ -111,7 +127,9 @@ class DependencyParser:
         elif text is None:
             with open(ifile, "r", encoding="utf-8") as f:
                 text = f.read()
+
         logging.info(f"Processing {ifile}...")
+
         ifile_prefix = ifile.split(".")[0]
         doc_spacy = self._depparse(text, ifile_prefix)  # type:ignore
         return doc_spacy
--- src/prev/querier.py
+++ src/prev/querier.py
@@ -5,6 +5,7 @@ from spacy.tokens.span import Span
 from spacy.tokens import Doc as Doc_spacy
 from spacy.matcher import DependencyMatcher
 
+
 class Querier:
     def __init__(self):
         # fmt: off
@@ -12,35 +13,38 @@ class Querier:
         # fmt: on
         self.patterns = self.generate_patterns(preps)
 
-    def generate_patterns(self, preps: List[str]) -> List[List[dict]]:  # {{{
-        patterns = []
-        patterns.append(
+    # {{{
+    def generate_patterns(self, preps: List[str]) -> List[List[dict]]:
+        patterns = [
             [
+                # 1. POS=ADP and DEP=prt,
+                #    e.g., His face lit up with pleasure. (Francis et al., 1996: 347)
+                # 2. POS=ADV and DEP=advmod,
+                #    e.g., The long-range goal must be to do away[advmod] with nuclear weapons altogether.
+                #    (Francis et al., 1996: 144)
                 {
                     "RIGHT_ID": "verb",
                     "RIGHT_ATTRS": {"TAG": {"REGEX": "^VB"}},
                 },
                 {
                     "LEFT_ID": "verb",
-                    "REL_OP": ">",  # A is the immediate head of B.
-                    "RIGHT_ID": "adverbial",
+                    "REL_OP": ">",
+                    # A is the immediate head of B.
+                    "RIGHT_ID": "prt-or-advmod",
                     "RIGHT_ATTRS": {
                         "POS": {"IN": ["ADP", "ADV"]},
                         "DEP": {"IN": ["prt", "advmod"]},
                     },
-                    # POS=ADP and DEP=prt, e.g., His face lit up with pleasure. (Francis et al., 1996: 347)
-                    # POS=ADV and DEP=advmod, e.g., The long-range goal must be to do away[advmod] with nuclear weapons altogether. (Francis et al., 1996: 144)
                 },
                 {
-                    "LEFT_ID": "adverbial",
-                    # B is a right immediate sibling of A, i.e., A and B have the same parent and A.i == B.i - 1.
+                    "LEFT_ID": "prt-or-advmod",
+                    # B is a right immediate sibling of A,
+                    # i.e., A and B have the same parent and A.i == B.i - 1.
                     "REL_OP": "$+",
                     "RIGHT_ID": "prep",
                     "RIGHT_ATTRS": {"ORTH": {"IN": preps}, "DEP": "prep"},
                 },
-            ]
-        )
-        patterns.append(
+            ],
             [
                 {
                     "RIGHT_ID": "verb",
@@ -49,20 +53,18 @@ class Querier:
                 {
                     "LEFT_ID": "verb",
                     "REL_OP": ".",
-                    "RIGHT_ID": "adverbial",
+                    "RIGHT_ID": "advmod",
                     "RIGHT_ATTRS": {
                         "DEP": "advmod",
                     },
                 },
                 {
-                    "LEFT_ID": "adverbial",
+                    "LEFT_ID": "advmod",
                     "REL_OP": "<+",
                     "RIGHT_ID": "prep",
                     "RIGHT_ATTRS": {"ORTH": {"IN": preps}, "DEP": "prep"},
                 },
-            ]
-        )
-        patterns.append(
+            ],
             [
                 {
                     "RIGHT_ID": "verb",
@@ -70,23 +72,53 @@ class Querier:
                 },
                 {
                     "LEFT_ID": "verb",
-                    "REL_OP": (
-                        ">+"
-                    ),  # B is a right immediate child of A, i.e., A > B and A.i == B.i -1
+                    "REL_OP": ">+",
+                    # B is a right immediate child of A, i.e., A > B and A.i == B.i -1
                     "RIGHT_ID": "prep",
                     "RIGHT_ATTRS": {"ORTH": {"IN": preps}, "DEP": "prep"},
                 },
-            ]
-        )
-        patterns.append(
+            ],
             [
+                # 1. I rummaged in my suitcase for a tie. (Francis et al., 1996: 234)
                 {
                     "RIGHT_ID": "verb",
                     "RIGHT_ATTRS": {"TAG": {"REGEX": "^VB"}},
                 },
                 {
                     "LEFT_ID": "verb",
-                    # A immediately precedes B, i.e., A.i == B.i - 1, and both are within the same dependency tree.
+                    "REL_OP": ">+",
+                    "RIGHT_ID": "intervening-prep",
+                    "RIGHT_ATTRS": {"DEP": "prep"},
+                },
+                {
+                    "LEFT_ID": "intervening-prep",
+                    "REL_OP": "$++",
+                    # A is a left sibling of B
+                    "RIGHT_ID": "prep",
+                    "RIGHT_ATTRS": {"ORTH": {"IN": preps}, "DEP": "prep"},
+                },
+            ],
+            [
+                # 1. Jones and his accomplice posed as police officers to gain entry to
+                #    the house. (Francis et al., 1996: 208)
+                # 2. She got up from her desk and motioned for Wade to follow her.
+                #    (Francis et al., 1996: 238)
+                # 3. They are pressing for the government to implement the electoral
+                #    promises of job creation and land reform as a first priority.
+                #    (Francis et al., 1996: 238)
+                # 4. All the women will be dying for you to make a mistake.
+                #    (Francis et al., 1996: 239)
+                # 5. He longed for the winter to be over. (Francis et al., 1996: 239)
+                # 6. I'll arrange for it to be sent direct to the properly when it is
+                #    unloaded. (Francis et al., 1996: 239)
+                {
+                    "RIGHT_ID": "verb",
+                    "RIGHT_ATTRS": {"TAG": {"REGEX": "^VB"}},
+                },
+                {
+                    "LEFT_ID": "verb",
+                    # A immediately precedes B,
+                    # i.e., A.i == B.i - 1, and both are within the same dependency tree.
                     "REL_OP": ".",
                     "RIGHT_ID": "prep",
                     "RIGHT_ATTRS": {"ORTH": {"IN": preps}, "DEP": "mark"},
@@ -95,21 +127,19 @@ class Querier:
                     "LEFT_ID": "prep",
                     # B is a right sibling of A, i.e., A and B have the same parent and A.i < B.i.
                     "REL_OP": "$++",
-                    "RIGHT_ID": "to-inf",
+                    "RIGHT_ID": "infinitive-to",
                     "RIGHT_ATTRS": {"ORTH": "to"},
                 },
-            ]
-        )
-        # Jones and his accomplice posed as police officers to gain entry to the house. (Francis et al., 1996: 208)
-        # She got up from her desk and motioned for Wade to follow her. (Francis et al., 1996: 238)
-        # They are pressing for the government to implement the electoral promises of job creation and land reform as a first priority. (Francis et al., 1996: 238)
-        # All the women will be dying for you to make a mistake. (Francis et al., 1996: 239)
-        # He longed for the winter to be over. (Francis et al., 1996: 239)
-        # I'll arrange for it to be sent direct to the properly when it is unloaded. (Francis et al., 1996: 239)
-        return patterns  # }}}
+            ],
+        ]
+        return patterns
+
+    # }}}
+
     def check_passive(self, verb_id: int, sent_spacy: Span) -> bool:
         # https://universaldependencies.org/en/feat/Voice.html#voice-voice
         is_passive = False
+
         if sent_spacy[verb_id].tag_ == "VBN":
             for child in sent_spacy[verb_id].lefts:
                 if child.dep_ == "auxpass":
@@ -117,40 +147,49 @@ class Querier:
                     break
         return is_passive
 
-    def parse_matches(self, matches: List[tuple], pattern: List[dict], sent_spacy:Span) -> str:
+    def parse_matches(self, matches: List[tuple], pattern: List[dict], sent_spacy: Span) -> str:
         # matches: [(match_id, [token_id1, token_id2, token_id3, token_id4]), ...]
         # each token_id corresponds to one pattern dict
         result = ""
+
         for match in matches:
             _, token_ids = match
             verb_id = token_ids[0]
+
             if self.check_passive(verb_id, sent_spacy):
                 continue
+
             for i in range(len(token_ids)):
                 right_id = pattern[i]["RIGHT_ID"]
                 node = sent_spacy[token_ids[i]]
                 result += f"{right_id}: {node.text}_{node.lemma_}, "
             result += "\n"
-        if result:
-            result = f"{sent_spacy.text}\n{result}"
-        return result.strip()
+        return result
 
-    def match_sent(self, sent_spacy:Span, matcher:DependencyMatcher):
+    def match_sent(self, sent_spacy: Span, matcher: DependencyMatcher):
         result = ""
+
         for pattern in self.patterns:
             if matcher.get("preps") is not None:
                 matcher.remove("preps")
             matcher.add("preps", [pattern])
+
             matches = matcher(sent_spacy)
             result += self.parse_matches(matches, pattern, sent_spacy)
-        return result
 
-    def match(self, doc_spacy:Doc_spacy, print_what:str):
+        if result:
+            result = f"{sent_spacy.text}\n{result}"
+        return result.strip()
+
+    def match(self, doc_spacy: Doc_spacy, print_what: str):
         assert print_what in ("matched", "unmatched"), f"Unexpected print_what: {print_what}"
+
         matcher = DependencyMatcher(doc_spacy.vocab)
         result = ""
+
         for sent_spacy in doc_spacy.sents:
             result_per_sent = self.match_sent(sent_spacy, matcher)
+
             if print_what == "matched" and result_per_sent:
                 result += result_per_sent + "\n\n"
             elif print_what == "unmatched" and not result_per_sent:
